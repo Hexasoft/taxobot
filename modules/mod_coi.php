2aeed80895c2e3cf1fb7a4c7a95395e0688c09a5
YannickPerret <yannick.perret@liris.cnrs.fr>
2021-07-11 19:21:44 +0200

    désactivation ITIS (problème chez eux) + ajout COI (non classification)

--- modules/mod_coi.php
+++ modules/mod_coi.php
@@ -8,21 +8,195 @@
 // déclaration du module
 function m_coi_init() {
   // permet classification, liens externes et accepte tous les domaines
-  return declare_module("coi", true, true, [ "oiseau" ], 10);
+  return declare_module("coi", false, true, [ "oiseau" ], 10);
 }
 
-// récupère les données générales liées à COI. Si $classif=TRUE récupère aussi les données de classification
+// récupère les données générales liées à COI
 function m_coi_infos(&$struct, $classif) {
+  $taxon = $struct['taxon']['nom'];
+
+  $url = "https://www.worldbirdnames.org/new/bow/";
+  $ret = get_data($url);
+  if ($ret === false) {
+    logs("COI: erreur réseau");
+    return false;
+  }
+  // on récupère l'URL cible
+  $tbl = explode("\n", $ret);
+  $url2 = false;
+  foreach($tbl as $ligne) {
+    if (strpos($ligne, "https://docs.google.com/") !== false) {
+      $url2 = preg_replace(',^.*src="https://docs.google.com/,', 'https://docs.google.com/', $ligne);
+      $url2 = preg_replace(',".*$,', '', $url2);
+      $url2 = str_replace('&amp;', '&', $url2);
+      break;
+    }
+  }
+  if (!$url2) {
+    logs("COI: lien suivant non trouvé");
+    return false;
+  }
+  $url2 = preg_replace(',/edit.*$,', '/export?format=csv', $url2);
+
+  $ret = get_data($url2);
+  if ($ret === false) {
+    logs("COI: échec de récupération au format CSV");
+    return false;
+  }
+  // saloperie de windows
+  $ret = str_replace("\r", "", $ret);
+  // saloperie de mal-formatage
+  $ret = preg_replace(",\n\",", '"', $ret);
+  // on parcours pour construire la classification jusqu'à l'espèce (et ssp)
+  $tbl = explode("\n", $ret);
+  $classe = "";
+  $ordre = "";
+  $famille = "";
+  $genre = "";
+  $espece = "";
+  $ssp = "";
+  $first = true;
+  $trouve = false;
+  $nbligne = 1;
+  foreach($tbl as $ligne) {
+    if ($first) {
+      $first = false;
+      continue;
+    }
+    $out = str_getcsv($ligne);
+    if (empty($out[3])) {
+      continue;
+    }
+    $test = (strtoupper($out[3]) == $out[3]);
+    $nb = str_word_count($out[3]);
+    // on trie selon la configuration
+    if ($test && ($nb == 1)) {
+      $classe = ucfirst(strtolower($out[3]));
+      $ordre = "";
+      $famille = "";
+      $genre = "";
+      $espece = "";
+      $ssp = "";
+    } else if ($test && ($nb == 2)) {
+      // présence de "ORDER" ?
+      $xpl = explode(" ", $out[3]);
+      if ($xpl[0] != "ORDER") {
+        // non reconnu
+        logs("COI: ligne non reconnue (ignorée)");
+        continue;
+      }
+      // extraction nom
+      $ordre = ucfirst(strtolower($xpl[1]));
+      $famille = "";
+      $genre = "";
+      $espece = "";
+      $ssp = "";
+    } else if (!$test && ($nb == 2)) {
+      // famille ou espèce
+      $xpl = explode(" ", $out[3]);
+      if ($xpl[0] == 'Family') {
+        $famille = $xpl[1];
+        $genre = "";
+        $espece = "";
+        $ssp = "";
+      }
+      // espèce
+      $espece = $out[3];
+      $ssp = "";
+    } else if (!$test && ($nb == 1)) {
+      $genre = $out[3];
+      $espece = "";
+      $ssp = "";
+    } else if (!$test && ($nb == 3)) {
+      // sous-espèce
+      $ssp = $out[3];
+    } else {
+      logs("COI: ligne non reconnue (2) (ignorée)");
+      continue;
+    }
+    // on regarde le taxon le plus précis courant
+    $courant = "";
+    $type = "";
+    $data = $out;
+    $tligne = $nbligne;
+    if (!empty($ssp)) {
+      $courant = $ssp;
+      $type = "sous-espèce";
+    } else if (!empty($espece)) {
+      $courant = $espece;
+      $type = "espece";
+    } else if (!empty($genre)) {
+      $courant = $genre;
+      $type = "genre";
+    } else if (!empty($famille)) {
+      $courant = $famille;
+      $type = "famille";
+    } else if (!empty($ordre)) {
+      $courant = $ordre;
+      $type = "ordre";
+    } else if (!empty($classe)) {
+      $courant = $classe;
+      $type = "classe";
+    } else {
+      logs("COI: type inconnu. Erreur");
+      return false;
+    }
+    $nbligne++;
+    // est-ce qu'on a trouvé notre taxon ?
+    if ($taxon == $courant) {
+      $trouve = true;
+      break;
+    }
+  }
+  if (!$trouve) {
+    logs("COI: taxon non trouvé");
+    return false;
+  }
+  
+  // on a trouvé le taxon chez COI
+  if (($type == "sous-espèce") || ($type == "espece") || ($type == "genre")) {
+    $auteur = $data[4];
+  } else {
+    $auteur = "";
+  }
+  
+  $blob = [];
+  $blob['nom'] = $courant;
+  if (!empty($auteur)) {
+    $blob['auteur'] = $auteur;
+  }
+  $blob['rang'] = $type;
+  $blob['ligne'] = $tligne;
+  $struct['liens']['coi'] = $blob;
+
+  if (!$classif) {
+    return true;
+  }
   return false;
 }
 
 // retourne les liens externes liés à COI (si présents)
 function m_coi_ext($struct) {
-  return false;
+  if (isset($struct['liens']['coi'])) {
+    $data = $struct['liens']['coi'];
+    $cdate = dates_recupere();
+    $texte = wp_met_italiques($data['nom'], $data['rang'], $struct['regne']);
+    if (isset($data['auteur'])) {
+      $texte .= " " . $data['auteur'];
+    }
+    $texte .= " (ligne " . $data['ligne'] . ")";
+    return "{{COI | | $texte | consulté le=$cdate }}";
+  } else {
+    return false;
+  }
 }
 
 // retourne les liens HTTP directs liés à COI (si présents)
 function m_coi_liens($struct) {
-  return false;
+  if (isset($struct['liens']['coi'])) {
+    return "<a href='https://www.worldbirdnames.org/bow/'>COI (ligne " . $struct['liens']['coi']['ligne'] . ")</a>";
+  } else {
+    return false;
+  }
 }
 
