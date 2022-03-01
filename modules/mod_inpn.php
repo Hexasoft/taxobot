75a1faf36685f87d4b52bfdea88aa36d15159db4
YannickPerret <yannick.perret@liris.cnrs.fr>
2022-03-01 13:09:59 +0100

    corr. INPN + support vernaculaires autres

--- modules/mod_inpn.php
+++ modules/mod_inpn.php
@@ -38,7 +38,7 @@ _embedded
     locationName (str, "France, "Réunion"…)
 
 */
-function m_inpn_infos_x(&$struct, $classif) {
+function m_inpn_infos(&$struct, $classif) {
   $taxon = $struct['taxon']['nom'];
   $url = "https://odata-inpn.mnhn.fr/taxa/names?nameFragment=" .
          str_replace(" ", "+", $taxon) . "&lteRank=SPECIES&embed=VALID_NAME&size=1";
@@ -58,7 +58,7 @@ function m_inpn_infos_x(&$struct, $classif) {
   }
   // requête complète
   $url = "https://odata-inpn.mnhn.fr/taxa/names?nameFragment=" .
-         str_replace(" ", "+", $taxon) . "&lteRank=SPECIES&embed=VALID_NAME&size=" .
+         str_replace(" ", "+", $taxon) . "&embed=VALID_NAME&size=" .
          $res->page->totalElements;
   $ret = get_data($url);
   if ($ret === false) {
@@ -84,7 +84,7 @@ function m_inpn_infos_x(&$struct, $classif) {
     $blob['nom'] = $taxon;
     $id = $blob['id'] = $r->taxrefId;
     $compl = $r->scientificName;
-    $blob['auteur'] = str_replace($blob['id'] . " ", "", $compl);
+    $blob['auteur'] = str_replace($blob['nom'] . " ", "", $compl);
     $ok = true;
     break;
   }
@@ -103,8 +103,17 @@ function m_inpn_infos_x(&$struct, $classif) {
   if ($res === null) {
     goto suite;
   }
-  foreach($res->xx as $r) {
-  
+  $ver = [];
+  foreach($res->_embedded->vernacularNames as $r) {
+    if ($r->langageId == "fra") {
+      $tmp = explode(", ", $r->name);
+      foreach($tmp as $t) {
+        $ver[] = $t;
+      }
+    }
+  }
+  if (!empty($ver)) {
+    $struct["vernaculaire"]['INPN'] = $ver;
   }
 
   // on l'ajoute
@@ -120,85 +129,6 @@ suite:
   return false;
 }
 
-// récupération des infos. Résultats à stocker dans $struct. Si $classif=TRUE doit
-// gérer la classification également
-function m_inpn_infos(&$struct, $classif) {
-  $taxon = $struct['taxon']['nom'];
-  $url = "https://inpn.mnhn.fr/inpn-web-services/autocomplete/especes/recherche?texte=" .
-          str_replace(" ", "+", $taxon) . "&taxref_groupe1=&taxref_groupe2=&max_resultats=1";
-  $ret = get_data($url);
-  if ($ret === false) {
-    logs("INPN: échec de récupération réseau");
-    return false;
-  }
-  $res = json_decode($ret);
-  if ($res === null) {
-    logs("INPN: échec de décodage des données");
-    return false;
-  }
-
-  if (!isset($res->response->numFound)) {
-    logs("INPN: pas de réponse pour cette recherche");
-    return false;
-  }
-  if ($res->response->numFound == 0) {
-    logs("INPN: pas de réponse pour ce taxon");
-    return false;
-  }
-  $max = $res->response->numFound;
-  // on refait la recherche avec le bon nombre de résultats
-  $url = "https://inpn.mnhn.fr/inpn-web-services/autocomplete/especes/recherche?texte=" .
-          str_replace(" ", "+", $taxon) . "&taxref_groupe1=&taxref_groupe2=&max_resultats=$max";
-  $ret = get_data($url);
-  if ($ret === false) {
-    logs("INPN: échec de récupération réseau (2)");
-    return false;
-  }
-  $res = json_decode($ret);
-  if ($res === null) {
-    logs("INPN: échec de décodage des données (2)");
-    return false;
-  }
-  if (!isset($res->response->docs)) {
-    echo "INPN: erreur pas response (2)\n";
-    return false;
-  }
-
-  // parcours
-  $ok = false;
-  $blob = [];
-  foreach($res->response->docs as $r) {
-    if (isset($r->lb_nom_valide) and ($r->lb_nom_valide == "$taxon")) {
-      $blob['nom'] = $taxon;
-      $blob['id'] = $r->cd_ref;
-      if (isset($r->lb_auteur_valide)) {
-        $blob['auteur'] = $r->lb_auteur_valide;
-      }
-      // si présent on extrait les les noms vernaculaires
-      if (isset($r->nom_vern) and !empty($r->nom_vern)) {
-        $tbl = explode(", ", $r->nom_vern);
-        $struct["vernaculaire"]['INPN'] = $tbl;
-      }
-      break;
-    }
-  }
-  
-  if (empty($blob)) {
-    logs("INPN: taxon non trouvé");
-    return false;
-  }
-  
-  // on l'ajoute
-  $struct['liens']['inpn'] = $blob;
-
-  // si pas plus loin, retour
-  if (!$classif) {
-    return true;
-  }
-  
-  // pas de classification
-  return false;
-}
 
 // génération des liens externes (modèles dans Voir aussi)
 function m_inpn_ext($struct) {
