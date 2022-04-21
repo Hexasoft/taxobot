3071bab1ddf82987b0e84ab2397a5fea5f58a86f
YannickPerret <yannick.perret@liris.cnrs.fr>
2022-04-21 15:52:59 +0200

    début de classification - incomplet

--- modules/mod_algaebase.php
+++ modules/mod_algaebase.php
@@ -67,6 +67,7 @@ $alg_rangs = [
   'empire' => 'empire',
   'kingdom' => 'royaume',
   'subkingdom' => 'sous-royaume',
+  'unknown' => 'non-classé',
 ];
 
 function alg_rang($rang) {
@@ -79,11 +80,11 @@ function alg_rang($rang) {
 }
 
 
-
 // déclaration du module
 function m_algaebase_init() {
-  return declare_module("algaebase", false, true, true);
+  return declare_module("algaebase", true, true, true);
 }
+
 
 // récupération des infos. Résultats à stocker dans $struct. Si $classif=TRUE doit
 // gérer la classification également
@@ -140,6 +141,8 @@ function m_algaebase_infos(&$struct, $classif) {
     foreach($res->result as $r) {
       $nom = $r->{"dwc:scientificName"};
       $nom = str_replace(" " . $r->{"dwc:scientificNameAuthorship"}, "", $nom);
+      // à faire : ajouter la détection de "non valide"
+      // 1. choisir valide exact 2. si synonyme redir (selon) 3. invalide (lien ext)
       if ($nom == $taxon) {
         // trouvé
         $found = $r;
@@ -175,7 +178,85 @@ function m_algaebase_infos(&$struct, $classif) {
     return true;
   }
   
-  return false;
+  // pour classification on met en place les infos
+  $struct['taxon'] = $struct['liens']['algaebase'];
+  // charge de la classif
+  $struct['classification'] = 'AlgaeBASE';
+  $struct['classification-taxobox'] = 'AlgaeBASE';
+  
+  // publication originale
+  if (isset($found->{"dcterms:bibliographicCitation"}) and !empty($found->{"dcterms:bibliographicCitation"})) {
+    $struct['originale'] = $found->{"dcterms:bibliographicCitation"};
+    $struct['originale'] = str_replace(['<i>','</i>'], "''", $struct['originale']);
+    if (isset($found->pdfs[0]->pdf_url)) {
+      $struct['originale'] .= " ([" . $found->pdfs[0]->pdf_url . " PDF])";
+    }
+  }
+  // diverses infos
+  $tbl = [];
+  if (isset($found->{"dwc:isMarine"}) and $found->{"dwc:isMarine"}) {
+    $tbl[] = "Cette espèce est marine";
+  }
+  if (isset($found->{"dwc:isFreshwater"}) and $found->{"dwc:isFreshwater"}) {
+    $tbl[] = "Cette espèce vit en eau douce";
+  }
+  if (isset($found->{"dwc:isTerrestrial"}) and $found->{"dwc:isTerrestrial"}) {
+    $tbl[] = "Cette espèce est terrestre";
+  }
+  if (isset($found->{"nameOrigin"}) and $found->{"nameOrigin"}) {
+    $tbl[] = "Étymologie : ''" . preg_replace("/[.][ ]*$/", "", $found->{"nameOrigin"}) . "''";
+  }
+  if (!empty($tbl)) {
+    $struct['description']['AlgaeBASE'] = $tbl;
+  }
+  
+  // on récupère la classification
+  $idgenre = $found->genusID;
+  $url = "https://api2.algaebase.org/v1.3/genus/$idgenre";
+  $header = [
+    "Accept: application/json",
+    "Origin: https://www.algaebase.org",
+    "DNT: 1",
+    "Referer: https://www.algaebase.org/",
+    "Sec-Fetch-Dest: empty",
+    "Sec-Fetch-Mode: cors",
+    "Sec-Fetch-Site: same-site",
+    "TE: trailers",
+    "abapikey: $key",
+  ];
+  $ret = get_data($url, $header);
+  if ($ret === false) {
+    logs("AlgaeBase: échec de récupération de la classification");
+    return false;
+  }
+  $res = json_decode($ret);
+  if ($res === false) {
+    logs("AlgaeBase: échec de décodage de la classification");
+    return false;
+  }
+  
+  if (!isset($res->classification)) {
+    logs("AlgaeBase: échec de d'extraction de la classification");
+    return false;
+  }
+  $tbl = [];
+  foreach($res->classification as $cl) {
+    if ($cl->{"dwc:taxonRank"} == 'empire') {
+      continue; // on n'utilise pas
+    }
+    $tmp = [];
+    $tmp['rang'] = alg_rang($cl->{"dwc:taxonRank"});
+    $nom = trim($cl->{"dwc:scientificName"});
+    $tmp['id'] = $cl->{"dwc:taxonID"};
+    $tmp['nom'] = trim(preg_replace("/" . $cl->{"dwc:taxonRank"} . "[ ]*/i", "", $nom));
+    $tbl[] = $tmp;
+  }
+  $struct['rangs'] = array_reverse($tbl);
+  
+  // la charte
+  $struct['regne'] = "algue"; //alg_regne($regne);
+  
+  return true;
 }
 
 // génération des liens externes (modèles dans Voir aussi)
