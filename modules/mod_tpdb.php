f1a6153921b3b618ce44eea02a47e7ea1e9b30fa
YannickPerret <yannick.perret@liris.cnrs.fr>
2022-05-06 15:01:19 +0200

    correction (changement) de mode de recherche

--- modules/mod_tpdb.php
+++ modules/mod_tpdb.php
@@ -9,6 +9,48 @@ function m_tpdb_init() {
   return declare_module("tpdb", false, true, true);
 }
 
+/*
+curl 'https://paleobiodb.org/data1.2/combined/auto.json?show=countries&name=Cytisus&type=cls&_=1651839047714' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0' -H 'Accept: application/json, text/javascript; q=0.01' -H 'Accept-Language: fr-FR,fr;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' -H 'Referer: https://paleobiodb.org/classic' -H 'Cookie: _ga=GA1.2.546965016.1651838604; _gid=GA1.2.272562783.1651838604' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin'
+
+→
+
+{
+	"elapsed_time": 0.00226,
+	"records": [
+		{
+			"oid": "txn:448323",
+			"nam": "Cytisus",
+			"vid": "var:448323",
+			"rnk": "genus",
+			"htn": "Magnoliopsida",
+			"noc": 0
+		},
+		{
+			"oid": "txn:448324",
+			"nam": "Cytisus florissantianus",
+			"vid": "var:448324",
+			"rnk": "species",
+			"htn": "Magnoliopsida",
+			"noc": 0
+		},
+		{
+			"oid": "txn:451494",
+			"nam": "Cytisus modestus",
+			"vid": "var:451494",
+			"rnk": "species",
+			"tdf": "recombined as",
+			"acn": "Ptelea modesta",
+			"htn": "Dicotyledoneae",
+			"noc": 0
+		}
+	]
+}
+
+
+
+*/
+
+
 // récupération des infos. Résultats à stocker dans $struct. Si $classif=TRUE doit
 // gérer la classification également
 function m_tpdb_infos(&$struct, $classif) {
@@ -17,31 +59,41 @@ function m_tpdb_infos(&$struct, $classif) {
   // on récupère la page de recherche (cookie)
   $url = "https://paleobiodb.org/classic/beginTaxonInfo";
   $ret = get_data($url);
-  // on cherche le taxon
-  $url = "https://paleobiodb.org/classic";
-  $post = "action=basicTaxonInfo&do_redirect=1&taxon_name=" . str_replace(" ", "+", $taxon) .
-          "&common_name=&author=&pubyr=&validity=valid&taxon_rank=species&exclude_taxon=";
-  $header = [ 'Referer: https://paleobiodb.org/classic/beginTaxonInfo',
-              'Sec-Fetch-Dest: document',
-              'Sec-Fetch-Mode: navigate',
+  $url = "https://paleobiodb.org/data1.2/combined/auto.json?show=countries&name=" .
+         str_replace(" ", "%20", $taxon) . "&type=cls&_=" . (time()*1000);
+  $header = [ 'Referer: https://paleobiodb.org/classic',
+              'Accept: application/json, text/javascript; q=0.01',
               'Sec-Fetch-Site: same-origin',
-              'Sec-Fetch-User: ?1',
-              'Content-Type: application/x-www-form-urlencoded',
-              'Origin: https://paleobiodb.org',
-              'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+              'Sec-Fetch-Dest: empty',
+              'Sec-Fetch-Mode: cors',
               'Upgrade-Insecure-Requests: 1'];
-  $ret = post_data_header($url, $post, $header, false);
+  $ret = get_data($url, $header);
   if ($ret === false) {
     logs("TPDB: échec de la recherche");
     return false;
   }
-
-  $tbl = explode("\n", $ret);
+  $res = json_decode($ret);
+  if (($res === false) or (!isset($res->records))) {
+    logs("TPDB: échec de l'analyse de la recherche");
+    return false;
+  }
+  if (empty($res->records)) {
+    logs("TPDB: taxon non trouvé");
+    return false;
+  }
+  
   $id = false;
-  foreach($tbl as $l) {
-    if (strpos($l, "taxon_no=") !== false) {
-      $id = trim(preg_replace("/^.*taxon_no=/", "", $l));
-      break;
+  $valid = true;
+  foreach($res->records as $rec) {
+    if ($rec->nam != $taxon) {
+      continue;
+    }
+    $tmp = explode(":", $rec->vid);
+    if (isset($tmp[1])) {
+      $id = $tmp[1];
+      if (isset($rec->acn)) {
+        $valid = false;
+      }
     }
   }
   
@@ -55,6 +107,9 @@ function m_tpdb_infos(&$struct, $classif) {
   // utilisation du nom de la classification : le format de leur page est trop pourri pour
   // tenter d'extraire le nom scientifique (et l'auteur) proprement. Tant pis.
   $struct['liens']['tpdb']['nom'] = $taxon;
+  if (!$valid) {
+    $struct['liens']['tpdb']['nv'] = true; // le modèle ne le gère pas, mais tant pis
+  }
 
   if (!$classif) {
     return true;
