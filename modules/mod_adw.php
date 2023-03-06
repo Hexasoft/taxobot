efb65cfbfe5b42ebb84115cd1fe1c7536a75b96f
LDWP <126354113+LDWP@users.noreply.github.com>
2023-03-06 14:39:39 +0000

    Révision générale
    Changements :
    * Nouveaux commentaires d'ensemble, plus précis pour les fonctions m_adw_infos() et m_adw_liens
    * Refactoring mineur : renommage des variables tbl -> lines, ligne -> line, ajout de la variable nextLine pour améliorer la lisibilité de la logique
    ** $acteurs vs $acteur sont sujets à refactoring ultérieur, si peu lisibles
    * Restructuration du traitement de la citation (auteurs et date) :
    ** Ajout de trois formats rendus différents pour les auteurs : "John Doe", "John Doe et Johnnie Doe" et "John Doe et al."
    ** Récupération puis rendu de la $date de la citation
    ** Réécriture partielle de la fonction m_adw_liens() pour faciliter la lisibilité et les maintenances

--- modules/mod_adw.php
+++ modules/mod_adw.php
@@ -1,16 +1,37 @@
 <?php
 
-/*
-  Module pour adw (non classification)
-*/
+/**
+ * Module - Animal Diversity Web (ADW)
+ *
+ * Ce module permet de récupérer des informations sur un taxon à partir du site ADW.
+ * Il génère des liens externes et des liens internes pour le site, mais il ne génère aucune classification.
+ *
+ */
 
-// déclaration du module
+/**
+ * Déclare le module ADW.
+ * 
+ * @return array|bool
+ */
 function m_adw_init() {
-  return declare_module("adw", false, true, true);
+    // Déclare le module ADW en spécifiant ses fonctionnalités.
+    return declare_module(
+        "adw",  // Nom du module.
+        false,  // Pas de lien de classification.
+        true,   // Peut générer des liens externes.
+        true    // Peut générer des liens internes.
+    );
 }
 
-// récupération des infos. Résultats à stocker dans $struct. Si $classif=TRUE doit
-// gérer la classification également
+/**
+ * Extraire les informations et les stocke dans une structure de données.
+ *
+ * @param array &$struct : cgstructure de données à remplir avec les informations.
+ * @param bool $classif : indique si la classification du taxon doit être gérée.
+ * @return bool True si les informations ont été récupérées avec succès, false sinon.
+ * 
+ */
+
 function m_adw_infos(&$struct, $classif) {
   $taxon = $struct['taxon']['nom'];
   
@@ -18,76 +39,133 @@ function m_adw_infos(&$struct, $classif) {
   $cible = str_replace(" ", "_", $taxon);
   $url .= $cible . "/";
   //$url .= $cible . "/classification/";
-  
+
+  // Échec récupération
   $ret = get_data($url);
   if ($ret === false) {
-    logs("ADW: echec de récupération réseau");
+    logs("ADW: échec de récupération réseau");
     return false;
   }
   
-  // non trouvé ?
+  // Taxon non trouvé
   $tmp = strpos($ret, ": Not Found<");
   if ($tmp !== false) {
     logs("ADW: taxon non trouvé");
     return false;
   }
   
-  // recherche de la citation (il faut parcourir car c'est sur la ligne suivante)
-  $tbl = explode("\n", $ret);
-  $cite = '';
+  $lines = explode("\n", $ret);
+  $citation = '';
   $nom = '';
-  foreach($tbl as $idx => $ligne) {
-    if (strpos($ligne, "To cite this page:") !== false) {
-      if (isset($tbl[$idx+1])) {
-        $l = $tbl[$idx+1];
-        $cite = trim(preg_replace("/([1-9][0-9][0-9][0-9])[.].*$/", '\1', $l));
-        continue;
+  $auteurs = '';
+  $premier_auteur = '';
+
+  /** Extraction de la citation (auteur[s] et date) et formatage
+   * @var string $citation : ligne "To cite this page: (...)" d'où on récupère des données pour le modèle ADW
+   * @var $nb_auteurs : le formatage de $auteurs dépend du nombre d'auteur[s]
+   * @var string $auteurs : auteur[s]
+   * formats : 1 auteur = $premier_auteur ; 2 = $premier_auteur ($pamatches) + deuxième auteur ($almatches) ; 3 ou plus = $premier_auteur et al.
+   * @var $date : YYYY ($dmatches)
+   */
+  foreach($lines as $idx => $line) {
+    if (strpos($line, "To cite this page:") !== false) {
+      if (isset($lines[$idx+1])) {
+        $nextLine = $lines[$idx+1];
+        $citation = trim($nextLine);
+        // Premier auteur
+        preg_match('/(?P<nom>\p{L}+),\s(?P<prenom1>[A-Z]\.)(?P<prenom2> [A-Z]\.)?/', $citation, $pamatches);
+        $premier_auteur = $pamatches['nom'] . ', ' . $pamatches['prenom1'];
+        if (!empty($pamatches['prenom2'])) {
+            $premier_auteur .= ' ' . $pamatches['prenom2'];
+        }
+        // Autre[s] auteur[s]
+        preg_match_all('/(?P<prenom1>[A-Z]\.)(?P<prenom2> [A-Z]\.)? (?P<nom>\p{L}+)/', $citation, $almatches);
+        $nb_auteurs = count($almatches[1]);
+        if ($nb_auteurs == 0) {
+          $auteurs = $premier_auteur;
+        } else {
+          if ($nb_auteurs > 2) {
+            $auteurs = $premier_auteur . ' {{et al.}}';
+          } elseif ($nb_auteurs == 1) {
+            $auteurs = $premier_auteur . ' et ' . $almatches['nom'][1] . ' ' . $almatches['prenom1'][1];
+            if (!empty($almatches['prenom2'])) {
+              $auteurs .= ' ' . $pamatches['prenom2'];
+            }
+          } else {
+            $auteurs =  $premier_auteur;
+          }
+        }
       }
-    }
-    if (strpos($ligne, '<h2 class="rank-') !== false) {
-      $x = preg_replace('/^[^<]*<[^>]*>/', '', $ligne);
-      $auteur = trim(preg_replace('/<.*$/', '', $ligne));
-      continue;
-    }
+      // Date
+        preg_match('/([1-9][0-9][0-9][0-9])[.].*$/', $citation, $dmatches);
+        $date = isset($dmatches[1]) ? $dmatches[1] : '';
+        break;
+    }  
   }
-  if (!empty($cite)) {
-    $struct['liens']['adw']['cite'] = $cite;
+  
+  // Stockage des données extraites
+  if (!empty($citation)) {
+    $struct['liens']['adw']['citation'] = $auteurs;
   }
-  if (!empty($auteur)) {
-    $struct['liens']['adw']['nom'] = $auteur;
+  if (!empty($date)) {
+    $struct['liens']['adw']['date'] = $date;
   } else {
     $struct['liens']['adw']['nom'] = $taxon; // on utilise le nom issu de la classification
   }
   $struct['liens']['adw']['id'] = $cible;
-  
-  if (!$classif == true) {
+
+  if (!$classif == true) { // syntaxe ? vers ($classif !== true) ou (!$classif) ?
     return true;
   }
+
   // ADW ne peut être une source de classification
   return false;
 }
 
-// génération des liens externes (modèles dans Voir aussi)
+/**
+ * Génère le modèle ADW de la section "Voir aussi"
+ * @param array $struct : correspond aux données récupérées par m_adw_infos()
+ * @return adw : retourne le modèle généré à partir des informations sur un taxon, la citation (date et auteur) de la page ADW, etc.
+ */
+
 function m_adw_ext($struct) {
-  $cdate = dates_recupere();
-  if (!isset($struct['liens']['adw']['id'])) {
+  $cdate = dates_recupere(); // Récupération de la date actuelle depuis outils.php
+  if (!isset($struct['liens']['adw']['id'])) { // Vérification de la présence de l'identifiant ADW dans la structure donnée
     return false;
   }
   $data = $struct['liens']['adw'];
+  $nomPageTaxon = $data['id'];
+  $description = '';
   if (isset($data['nom'])) {
-    $txt = wp_met_italiques($data['nom'], $struct['taxon']['rang'], $struct['regne']);
+    $description = wp_met_italiques($data['nom'], $struct['taxon']['rang'], $struct['regne']);
   } else {
-    $txt = wp_met_italiques($struct['taxon']['nom'], $struct['taxon']['rang'], $struct['regne']);
+    $description = wp_met_italiques($struct['taxon']['nom'], $struct['taxon']['rang'], $struct['regne']);
   }
-  if (isset($data['cite'])) {
-    $cite = "auteur=" . $data['cite'] . " | ";
-  } else {
-    $cite = "";
-  }
-  return "{{ADW | " . $data['id'] . " | " . $txt . " | $cite" . "consulté le=$cdate }}";
-}
 
-// génération de liens vers les éléments (pour partie aide/debug de l'interface)
+  $auteur = isset($data['citation']) ? $data['citation'] : '';
+  $date = isset($data['date']) ? $data['date'] : '';
+  
+  $adw = "{{ADW | $nomPageTaxon | $description | consulté le=$cdate"; // Construction du modèle ADW et de ses paramètres, si non vides.
+  
+  /**
+  * Fonctionnalité qui pourrait être proposée (cmd/web), générée par outils.php (par défaut ~ le nom du module, "adw")
+  * ou correspondre à une concaténation "ADW" + nom[x] issu de $pamatches (premier auteur) ou "ADW" + $cdate
+  */
+  // if (!empty($ancre)) { $adw .= " | ancre=$ancre"; }
+ 
+  if (!empty($auteur)) {
+    $adw .= " | auteur=$auteur";
+  }
+  if (!empty($date)) {
+    $adw .= " | date=$date";
+  }
+  
+  $adw .= " }}"; // Fermeture du modèle ADW
+  
+  return $adw; // Retourne le modèle ADW généré
+} 
+
+// Génération de liens vers les éléments (pour partie aide/debug de l'interface)
 function m_adw_liens($struct) {
   if (isset($struct['liens']['adw']['id'])) {
     return "<a href='https://animaldiversity.org/accounts/" . $struct['liens']['adw']['id'] .
@@ -96,4 +174,3 @@ function m_adw_liens($struct) {
     return false;
   }
 }
-
