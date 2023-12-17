0512e2daa1754415d45b4fba7be2b0866aa509ef
LDWP <wpld2021@gmail.com>
2023-12-17 15:56:41 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -163,9 +163,8 @@ require_once join(DIRECTORY_SEPARATOR, array(dirname(__FILE__), '..', 'rendu.php
     </main>
     <!-- Pied de page -->
     <footer id="footer">
-        <p><a href="https://fr.wikipedia.org/wiki/Projet:Biologie/Taxobot" target="_blank">Taxobot</a> 2023 - Hébergé sur Toolforge par <a href="https://wikitech.wikimedia.org/wiki/Help:Cloud_Services_introduction" target="_blank">Wikimedia Cloud Services</a></p>
+        <p><a href="https://fr.wikipedia.org/wiki/Projet:Biologie/Taxobot" target="_blank">Taxobot</a> 2023 - Hébergé sur Toolforge par <a href="https://wikitech.wikimedia.org/wiki/Help:Cloud_Services_introduction" target="_blank">Wikimedia Cloud Services</a> - <a href="https://github.com/Hexasoft/taxobot/tree/main/web/LICENSE.md" target="_blank">Licences</a></p>
     </footer>
-
                 <!-- JS -->
         <script src="js/options.js"></script>
         <script src="js/copy.js"></script>
